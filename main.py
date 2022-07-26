e5d8c9d8f06296b502d4170c9397a015ae5a94ed
ByFlown <byflown@gmail.com>
2022-07-26 17:57:17 +0200

    Initial

--- main.py
+++ main.py
@@ -4,17 +4,18 @@ realTimeDate = [datetime.datetime.now().day, datetime.datetime.now().month, date
 realTimeYear = datetime.datetime.now().year
 
 date = []
+isInputInPast = False
 
 # Get and Check the year of the Input
 # HEHEHEHE
 
 year = int(input("Year: "))
 
-if year >= datetime.datetime.now().year:
+if year >= realTimeDate[2]:
     date.append(year)
 else:
-    print("Input 'Year' can't be in the past!")
-    exit()
+    date.append(year)
+    isInputInPast = True
 
 isLeapYear = float(year / 4).is_integer() and not float(year / 100).is_integer()
 
@@ -26,9 +27,13 @@ else:
 # Get and Check the month of the Input
 month = int(input("Month: "))
 
-if 0 < month <= 12:
+if 0 < month <= 12 and month < realTimeDate[1] and year <= realTimeDate[2]:
     date.append(month)
-else:
+    isInputInPast = True
+
+if 0 < month <= 12 and not isInputInPast:
+    date.append(month)
+elif month > 12 or month <= 0:
     print("Input isn't in range of 1-12!")
     exit()
 
@@ -36,12 +41,15 @@ else:
 # Get and Check the day of the Input
 day = int(input("Day: "))
 
-if 0 < day <= months[month - 1]:
+if 0 < day <= months[month - 1] and day < realTimeDate[0] and year <= realTimeDate[2] and month <= realTimeDate[1]:
     date.append(day)
-else:
+    isInputInPast = True
+
+if 0 < day <= months[month - 1] and not isInputInPast:
+    date.append(day)
+elif day <= 0 or day > months[month - 1]:
     print("Input isn't in range of 1-" + months[month - 1].__str__() + "!")
     exit()
-
 
 def CalculateDaysRange():
 
@@ -57,31 +65,60 @@ def CalculateDaysRange():
     currMonth = realTimeDate[1]
     currYear = realTimeDate[2]
 
-    while currDay != date[2] or currMonth != date[1] or currYear != date[0]:
+    while not currDay == date[2] or not currMonth == date[1] or not currYear == date[0]:
 
-        if currDay < _months[currMonth - 1]:
-            currDay += 1
-            days += 1
-            # print("CurrDay: " + currDay.__str__())
-        else:
-            currDay = 0
+        if not isInputInPast:
 
-            if currMonth >= 12:
-                currMonth = 1
-                currYear += 1
-
-                leapYear = float(currYear / 4).is_integer() and not float(currYear / 100).is_integer()
-
-                if not leapYear:
-                    _months = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-                else:
-                    _months = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-
+            if currDay < _months[currMonth - 1]:
+                currDay += 1
+                days += 1
+                # print("CurrDay: " + currDay.__str__())
             else:
-                currMonth += 1
-                # print("CurrMonth: " + currMonth.__str__())
+                currDay = 0
 
-    print("Days until " + date[0].__str__() + "." + date[1].__str__() + "." + date[2].__str__() + ": " + days.__str__())
+                if currMonth >= 12:
+                    currMonth = 1
+                    currYear += 1
+
+                    leapYear = float(currYear / 4).is_integer() and not float(currYear / 100).is_integer()
+
+                    if not leapYear:
+                        _months = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+                    else:
+                        _months = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+                else:
+                    currMonth += 1
+                    # print("CurrMonth: " + currMonth.__str__())
+
+        else:
+            if currDay > 0:
+
+                currDay -= 1
+                days += 1
+                # print("CurrDay: " + currDay.__str__())
+            else:
+                currDay = _months[currMonth - 1]
+
+                if currMonth <= 1:
+                    currMonth = 12
+                    currYear -= 1
+
+                    leapYear = float(currYear / 4).is_integer() and not float(currYear / 100).is_integer()
+
+                    if not leapYear:
+                        _months = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+                    else:
+                        _months = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+                else:
+                    currMonth -= 1
+                    # print("CurrMonth: " + currMonth.__str__())
+
+    if not isInputInPast:
+        print(date[0].__str__() + "." + date[1].__str__() + "." + date[2].__str__() + " is in " + days.__str__() + " days.")
+    else:
+        print(date[0].__str__() + "." + date[1].__str__() + "." + date[2].__str__() + " was " + days.__str__() + " days ago.")
 
 
 CalculateDaysRange()
